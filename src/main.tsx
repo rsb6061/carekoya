bf9dd9e45c9d9ac23e0d20652acc222582a79a2c
rsb6061 <myersrebeccal@gmail.com>
2026-09-25 17:51:11 -0400

    Route Maryland caregiver acquisition page

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
+import { MarylandCaregiverPage } from './MarylandCaregiverPage';
 import './styles.css';
 
+const path=window.location.pathname;
+const Root=path.startsWith('/caregiver-jobs/maryland')?MarylandCaregiverPage:App;
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode><App /></React.StrictMode>
+  <React.StrictMode><Root /></React.StrictMode>
 );
