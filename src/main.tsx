777c0e79ffab9ddf7cf610ce346f7cf8e766ae13
rsb6061 <myersrebeccal@gmail.com>
2026-09-25 18:01:41 -0400

    Route school claim and placement dashboard

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
 import { MarylandCaregiverPage } from './MarylandCaregiverPage';
+import { SchoolProgramPage, SchoolAuth, SchoolDashboard } from './SchoolPortal';
 import './styles.css';
 
 const path=window.location.pathname;
-const Root=(path.startsWith('/caregiver-jobs/maryland')||path.startsWith('/join/'))?MarylandCaregiverPage:App;
+const Root=path.startsWith('/school-auth')?SchoolAuth:path.startsWith('/school-dashboard')?SchoolDashboard:path.startsWith('/school/')?SchoolProgramPage:(path.startsWith('/caregiver-jobs/maryland')||path.startsWith('/join/'))?MarylandCaregiverPage:App;
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode><Root /></React.StrictMode>
