bfae49eed40ba8aafdbda4e5fa7a66ca53fbbe52
rsb6061 <myersrebeccal@gmail.com>
2026-09-25 17:57:50 -0400

    Route school referral links to caregiver signup

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { MarylandCaregiverPage } from './MarylandCaregiverPage';
 import './styles.css';
 
 const path=window.location.pathname;
-const Root=path.startsWith('/caregiver-jobs/maryland')?MarylandCaregiverPage:App;
+const Root=(path.startsWith('/caregiver-jobs/maryland')||path.startsWith('/join/'))?MarylandCaregiverPage:App;
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode><Root /></React.StrictMode>
