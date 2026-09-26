8fdccdd603685577ac2990ecaf6eac87de95bb69
rsb6061 <myersrebeccal@gmail.com>
2026-09-25 23:12:52 -0400

    Route caregiver resume acquisition funnel

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ import { MarylandSchoolsPage } from './MarylandSchoolsPage';
 import { TrainingOrganizationPage } from './TrainingOrganizationPage';
 import { EmployerRecruitingPage, AboutCareJoysPage } from './PublicInfoPages';
 import { HowToBecomeCaregiverMarylandPage } from './CaregiverResourcePage';
+import { CaregiverResumePage } from './CaregiverResumePage';
 
 type FormKind = 'employer' | 'caregiver' | 'school' | null;
 
@@ -124,6 +125,7 @@ function IntakeModal({
 
 export function App() {
   if (window.location.pathname.startsWith('/hire-caregivers/maryland')) return <EmployerRecruitingPage />;
+  if (window.location.pathname.startsWith('/caregiver-resume')) return <CaregiverResumePage />;
   if (window.location.pathname.startsWith('/resources/how-to-become-a-caregiver-in-maryland')) return <HowToBecomeCaregiverMarylandPage />;
   if (window.location.pathname === '/about' || window.location.pathname.startsWith('/about/')) return <AboutCareJoysPage />;
   if (window.location.pathname.startsWith('/activate')) return <CaregiverActivation />;
