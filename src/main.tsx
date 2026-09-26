6298fb7e7ae7d42dbd83dcee0d1bf512e51d0bab
rsb6061 <myersrebeccal@gmail.com>
2026-09-26 12:21:31 -0400

    Wrap CareJoys in caregiver Auth0 provider

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import { App } from './App';
 import { MarylandCaregiverPage } from './MarylandCaregiverPage';
 import { SchoolProgramPage, SchoolAuth, SchoolDashboard } from './SchoolPortal';
+import { CaregiverAuthProvider } from './caregiverAuth';
 import './styles.css';
 
 const path=window.location.pathname;
 const Root=path.startsWith('/school-auth')?SchoolAuth:path.startsWith('/school-dashboard')?SchoolDashboard:path.startsWith('/school/')?SchoolProgramPage:(path.startsWith('/caregiver-jobs/maryland')||path.startsWith('/join/'))?MarylandCaregiverPage:App;
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode><Root /></React.StrictMode>
+  <React.StrictMode><CaregiverAuthProvider><Root /></CaregiverAuthProvider></React.StrictMode>
 );
